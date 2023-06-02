834e0f22ea5687ee3413794135eb0e1884728319
iamharika <nharika203@gmail.com>
2023-06-02 16:43:19 +0530

    final commit

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 function myFunction() {
     window.location.href = "dashboard.html";
   }
+function myProduct() {
+    window.location.href = "product.html";
+}
+function myPayment() {
+  window.location.href = "payment.html";
+}
+document.getElementById("payment-form").addEventListener("submit", function(event) {
+  event.preventDefault();
+  alert("Payment sent successfully!");
+});
